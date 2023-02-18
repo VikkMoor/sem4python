8964373a8506986586fdb14efd9cbe525d82e7f5
Victoria Moor <victimofuroboros@gmail.com>
2023-02-18 18:24:56 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -3,7 +3,7 @@
 my_list = list(map(int, input("Enter random numbers separated by spaces: ").split()))
 print(f"Original list: {my_list}")
 new_list = []
-for i in my_list:
-    if i not in new_list:
-        new_list.append(i)
+for el in my_list:
+    if el not in new_list:
+        new_list.append(el)
 print(f"List of unique elements: {new_list}")
